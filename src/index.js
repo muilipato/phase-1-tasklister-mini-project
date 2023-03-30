4eb005e79ec121fe2847899481f3926ff10daf80
Patrick Maweu <muilipato@gmail.com>
2023-03-30 13:29:30 +0300

    Done assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   // your code here
+  const form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    taskList(e.target.new_task_description.value)
+    form.reset()
+  })
+
+
+  function taskList (todo){
+    let li = document.createElement("li")
+    li.textContent = `${todo} `
+    
+    document.querySelector("#tasks").appendChild(li)
+
+  }
+
+
+
+  
 });
